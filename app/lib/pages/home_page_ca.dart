854ae1f59bc4923d18149c3b798b23336c9ea739
Andravi <andravi75@gmail.com>
2023-06-23 12:00:53 -0300

    RFs G022 and G023 Implemented

--- app/lib/pages/home_page_ca.dart
+++ app/lib/pages/home_page_ca.dart
@@ -1,5 +1,6 @@
 import 'package:app/pages/register_book.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -49,6 +50,106 @@ class _HomePageCaState extends State<HomePageCa> {
         child: Scaffold(
           key: scaffoldKey,
           backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          endDrawer: SafeArea(
+            top: true,
+            bottom: true,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Drawer(
+                elevation: 16,
+                child: SizedBox(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Align(
+                        alignment: AlignmentDirectional(1, -1),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          child: IconButton(
+                            onPressed: () {
+                              if (scaffoldKey.currentState!.isDrawerOpen || scaffoldKey.currentState!.isEndDrawerOpen) {
+                                Navigator.pop(context);
+                              }
+                            },
+                            icon: Icon(
+                              Icons.cancel_outlined,
+                              size: 24.0,
+                              color: FlutterFlowTheme.of(context).alternate,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        decoration: BoxDecoration(
+                          color: FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(16, 8, 0, 0),
+                              child: InkWell(
+                                onTap: () async {
+                                  await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop', true);
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.power_settings_new_rounded,
+                                      color: FlutterFlowTheme.of(context).secondaryText, //
+                                      size: 24,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        'Sair do app',
+                                        style: FlutterFlowTheme.of(context).bodyLarge,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(16, 16, 0, 0),
+                              child: InkWell(
+                                onTap: () async {
+                                  context.read<AuthService>().logout(context);
+                                },
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.logout,
+                                      color: FlutterFlowTheme.of(context).secondaryText,
+                                      size: 24,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        'Fazer logout',
+                                        style: FlutterFlowTheme.of(context).bodyLarge,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
           body: SafeArea(
             top: true,
             child: Align(
@@ -152,7 +253,7 @@ class _HomePageCaState extends State<HomePageCa> {
                               onPressed: () {
                                 setState(
                                   () {
-                                    context.read<AuthService>().logout(context);
+                                    scaffoldKey.currentState!.openEndDrawer();
                                   },
                                 );
                               },
